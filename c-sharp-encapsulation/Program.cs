44581ceb51f5b384812860a974358d3407dcdc9c
omranali2111 <aljabri1234@hotmail.com>
2023-06-17 19:52:49 +0400

    Question 2# Design a class called Rectangle

--- c-sharp-encapsulation/Program.cs
+++ c-sharp-encapsulation/Program.cs
@@ -1,19 +1,23 @@
 ﻿using c_sharp_encapsulation;
-
+using static System.Console;
 internal class Program
 {
     private static void Main(string[] args)
     {
         Person person1=new Person("omran",27,"seeb");
-       
-
+    
         person1.SetName("ali");
-
         person1.PrintPersonDetails();
+
+        WriteLine("###########################################");
+        Rectangle rectangle = new Rectangle(3.2, 4.3);
+        rectangle.PrintRectangleDetails();
     }
 
-
    
+    
+
+
 
 
 
---
+++ c-sharp-encapsulation/Rectangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_encapsulation
+{
+    internal class Rectangle
+    {
+        /*
+        Question 2# Design a class called Rectangle with the following private properties:
+        length(double)
+        width(double)
+        Include public methods to calculate the area and perimeter of the rectangle based on its length and width.
+
+        */
+
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double CalculateArea()
+        {
+            return length * width;
+        }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * (length + width);
+        }
+
+        public void PrintRectangleDetails()
+        {
+            Console.WriteLine("Length: " + length);
+            Console.WriteLine("Width: " + width);
+            Console.WriteLine("Area: " + CalculateArea());
+            Console.WriteLine("Perimeter: " + CalculatePerimeter());
+        }
+    }
+}
